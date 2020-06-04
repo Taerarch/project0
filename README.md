3d1b30c492d56270f0d1961c255a6b6b024afa12
Matthew <matthew.tudman@gmail.com>
2020-06-04 15:50:22 +1000

    Second commit Final Product Fingers Crossed

--- README.md
+++ README.md
@@ -1,104 +1,48 @@
-# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) Project #0: The Game
-
-### Overview
-
-Let's start out with something fun - **a game!**
-
-**We'll be making Tic Tac Toe**, a game that takes seconds to learn but minutes to master! Everyone will get a chance to **be creative**, and work through some really **tough programming challenges** to get your feet wet in the world of web development.
-
-> "hand me your phone," load up the game, and play a quick round!
-
-**You will be working individually for this project**, but we'll be guiding you along the process and helping as you go. Show us what you've got!
-
----
-
-### What You've Learned
-
-By the time you submit this project, you will have covered new ground in many of the big themes of the course:
-
-- **Command Line**: Practice interacting with the computer and navigating the filesystem from the command line.
-- **Source Control**: Manage and interact with a git repository to store changes to code.
-- **Programming Fundamentals**: Work with array, objects, event handlers and callbacks, while learning how to strategically solve problems and resolve errors.
-- **Web Fundamentals**:  Learn how communication happens over the internet, and how to structure, style, and animate documents within a browser. Also learn how to respond to actions your users take and the data they input into the browser.
-- **Browser Applications**:  Dive into CSS, Sass, and how to use libraries and frameworks to get lots of style for free.
-- **Deployment**: Host a static web site in a managed hosting environment.
-- **Products and Teams**: Document your code and your code repository so others understand what you've built.
+# Project 0 - Tic Tac Toe
 
 
----
 
-### Big Goals
+The core of the mechanics behind the game is a set of default buttons that upon being clicked a character dependant on the current turn is placed inside. An if statement checks if there is already a character inside the button and prevents a second input.
 
-* **Build a web application from scratch**, without a starter codebase
-* Use your programming skills to **map out the game logic for a simple game like Tic Tac Toe**
-* **Separate HTML, CSS, and JavaScript files** in your application
-* Build an application **to a spec that someone else gives you**
-* **Build a dynamic game that allows two players to compete**
-* **Craft a ``readme.md`` file that explains your app** to the world
+Then we use a nested for loop to extract the text inside each button to create an array for easier access when testing for the win conditions. Next a single loop goes through each different win condition breaking them up into horizontal, vertical and diagonal win conditions.
 
----
 
-### Technical Requirements
+#### Winning
 
-Your app must:
+Horizontal loops through the generated array selecting the elements in groups of three to see if all the elements in the row are the same. Vertical again loops through the array but selects elements two apart from each other to compare the elements in a column. The diagonals had to be hard coded comparing element 1, 5, 9 or 3, 5, 7.
 
-* **Render a game board in the browser**
-* **Switch turns** between X and O (or whichever markers you select)
-* **Visually display which side won** if a player gets three in a row or show a draw/"cat’s game" if neither wins
-* **Include separate HTML / CSS / JavaScript files**
-* Stick with **KISS (Keep It Simple Stupid)** and **DRY (Don't Repeat Yourself)** principles
-* Use **Javascript** for **DOM manipulation**
-* **Deploy your game online**, where the rest of the world can access it
-* Use **semantic markup** for HTML and CSS (adhere to best practices)
 
----
+If any of these return true the win class is added to those squares through another reference through jQuery which then causes a small transition into different a colour and border for the winning squares. When a win is found a number counter is increased depending on the current turn and a message is added below the squares declaring the winner.
 
-### Bonus
 
-These are for extra credit! Don't focus on these until you've hit the core requirements.
+#### Different Characters
 
-* Keep track of **multiple game rounds** with a win counter
-* Allow players to **customize their tokens** (X, O, name, picture, etc)
-* **Get inventive with your styling**, e.g. use hover effects or animations to spiff things up
-* **Use LocalStorage** to persist data locally to allow games to continue after page refresh or loss of internet connectivity
-* **Support custom board sizes**: default is 3x3 but you could allow users to choose a larger board
-* **Support networked multiplayer**: https://www.firebase.com/ has a nice quickstart guide
-* **TRICKIEST**: Create an AI opponent: teach Javascript to play an unbeatable game against you
+The game when activating the game buttons actually chooses the character based on a variable that gets updated by these buttons. All the previous code works based on comparison between the actual text while the colour and who's turn it is is based on invisible but unchanging variables X and O. (Argument could be made to change these to PlayerOne and PlayerTwo.)
 
----
+This does however lead to an unintended feature that when you change character mid-game the different characters from the same player don't actually interact with each other.
 
-### Necessary Deliverables
 
-* A **working game, built by you**, hosted somewhere on the internet
-* A **link to your hosted working game** in the URL section of your Github repo
-* A **git repository hosted on Github**, with a link to your hosted game, and frequent commits dating back to the very beginning of the project
-* **A ``readme.md`` file** with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+#### Resetting
 
----
+The reset button goes through each button and removes the win class as well as emptying the text inside.
 
-### Suggested Ways to Get Started
+The reset score button does the same as well as emptying the win counters to then update them.
 
-* **Break the project down into different components** (data, presentation, views, style, DOM manipulation) and brainstorm each component individually. Use whiteboards!
-* **Use your Development Tools** (console.log, inspector, alert statements, etc) to debug and solve problems
-* Work through the lessons in class, **ask questions and come to office hours** when you need to. Think about adding relevant code to your Tic Tac Toe game each night, instead of, you know... _procrastinating_.
-* **Commit early, commit often.** Don’t be afraid to break something because you can always go back in time to a previous version.
-* **Check out Tutorial and Documentation resources** (jQuery tutorial) at home to better understand what you’ll be getting into.
-* **Don’t be afraid to write code that you know you will have to remove later.** Create temporary elements (buttons, links, etc) that trigger events if real data is not available. For example, if you’re trying to figure out how to change some text when the game is over but you haven’t solved the win/lose game logic, you can create a button to simulate that until then.
 
----
+#### Saving
 
-### Useful Resources
+Upon unloading the page a nested loop is run that creates a variable for each of the buttons with their inside text as well as what character was being used currently. When it is reloaded first the character is reselected and then the characters are reinserted into the buttons. Then it checks to see if the text inside is the same as the chosen character, if so the 'X' or 'O' class is assigned to that button.
 
-* **[MDN Javascript Docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript)** _(a great reference for all things Vanilla Javascript)_
-* **[jQuery Docs](http://api.jquery.com)**
-* **[Github Pages](https://pages.github.com)** _(for hosting your game)_
+The win counters as well as whether or not the computer is playing is also saved in a similar way.
 
----
+#### Computer opponent
 
-### If You Finish Early
+The computer opponent is programmed to take the middle square if the player does not.
 
-We invite you to work on any or all of the following:
+After that it prioritises winning by looking to see if any two of its squares are in a line with an empty square which it then clicks on. This was labelled as the offensive section
 
-* A more advanced game (Memory? Battleship? Connect Four?)
-* Your Github portfolio site
-* Any other front-end project that interests you
+Failing that it will look to block the human player if they have any combination of two squares with a line on a third. This was labelled in the comments as the defensive section.
+
+If the human makes a move not expected and the computer is not required to block and cannot immediately win it will place a random move from top left to bottom right.
+
+There is a button to turn off the Computer opponent by making a variable false which is checked whenever a button is clicked on the grid.
--- js/project.js
+++ js/project.js
@@ -1,14 +1,16 @@
 
-
-
+// Confetti
+//
 
 
 let markerX = 'X';
 let markerO = 'O';
 let turnSwap = 'X';
-// let winValue = ''
 let win = false;
 let board = [];
+let firstWins = 0;
+let secondWins = 0;
+let togglePVP = true;
 
 
 const change = function() {
@@ -21,46 +23,39 @@ const change = function() {
       turnSwap = 'X';
     }
     checkWin();
+    if (togglePVP === false) { bot() };
+    board = [];
   }
 }
-
-
-// play again button?
-// change color of winning line
 
 
 const horizontalWin = function() {
   for (let i = 0; i < 3; i++) {
     if (board[0 + (i*3)] === board[2 + (i*3)] && board[0 + (i*3)] === board[1 + (i*3)] && board[0 + (i*3)] != '') {
-      console.log('You win!');
       $(`.Row${i+1} button`).addClass('win');
-      // winValue = $(`.Row${i+1} button`).text(); // That mess of code checks the text of the first
-      // $('.winOutput').append(`<li>${winValue[0]} wins! Congratulations!</li>`);
-      // win = true;
       winMessage(($(`.Row${i+1} button`).text())[0]);
     }
   }
 }
 
+
 const verticalWin = function() {
   for (let i = 0; i < 3; i++) {
     if (board[0 + (i)] === board[6 + (i)] && board[0 + (i)] === board[3 + (i)] && board[i] != '') {
-      console.log('You win!');
       $(`.Col${i+1}`).addClass('win');
-      winMessage(($(`.Col${i+1}`).text())[0])
+      winMessage(($(`.Col${i+1}`).text())[0]);
     }
   }
 }
 
+
 const diagonalWin = function() {
   if (board[0] === board[4] && board[0] === board[8] && board[0] != '') {
-    console.log("You win!");
     $(`.Row1 .Col1`).addClass('win');
     $(`.Row2 .Col2`).addClass('win');
     $(`.Row3 .Col3`).addClass('win');
     winMessage(($(`.Row1 button`).text())[0]);
   } else if (board[2] === board[4] && board[2] === board[6] && board[2] != '') {
-    console.log("You win!");
     $(`.Row1 .Col3`).addClass('win');
     $(`.Row2 .Col2`).addClass('win');
     $(`.Row3 .Col1`).addClass('win');
@@ -72,7 +67,11 @@ const diagonalWin = function() {
 const winMessage = function(input) {
   $('.winOutput').append(`<li>${input} wins! Congratulations!</li>`);
   win = true;
+  if (turnSwap === 'X') { secondWins += 1} else { firstWins += 1};
+  $('#left').text(firstWins);
+  $('#right').text(secondWins);
 }
+
 
 const checkWin = function() {
   if (win === false) {
@@ -84,7 +83,6 @@ const checkWin = function() {
     horizontalWin();
     verticalWin();
     diagonalWin();
-    board = [];
   }
 }
 
@@ -99,9 +97,15 @@ const reset = function() {
 
 
 
-
 $('button').on('click', change);
-$('#reset').on('click', reset);
+$('.reset').on('click', reset);
+
+$('#scores').on('click', function() {
+  firstWins = 0;
+  secondWins = 0;
+  $('#left').text(firstWins);
+  $('#right').text(secondWins);
+});
 
 
 $('#changeXOne').on('click', function() { markerX = 'X'});
@@ -111,5 +115,299 @@ $('#changeXFour').on('click', function() { markerX = 'T'});
 
 $('#changeXFive').on('click', function() { markerO = 'O'});
 $('#changeXSix').on('click', function() { markerO = 'S'});
-$('#changeXSeven').on('click', function() { markerO = '&'});
+$('#changeXSeven').on('click', function() { markerO = 'G'});
 $('#changeXEight').on('click', function() { markerO = '#'});
+
+$('#pvp').on('click', function() { togglePVP = true})
+$('#pve').on('click', function() { togglePVP = false})
+
+
+
+
+
+const saveGameState = function() {
+  localStorage["playerOne"] = markerX;
+  localStorage["playerTwo"] = markerO;
+  localStorage["turnKeep"] = turnSwap;
+  localStorage["storeFirstWins"] = firstWins;
+  localStorage["storeSecondWins"] = secondWins;
+  localStorage["storePVP"] = togglePVP;
+
+  for (let x = 0; x < 3; x++) {
+    for (let i = 0; i < 3; i++) {
+      localStorage[`square${i+1+(x*3)}`] = $(`.Row${x+1} .Col${i+1}`).text();
+    }
+  }
+}
+
+// $('window').on("unload", saveGameState())
+
+const resumeGameData = function() {
+  togglePVP = localStorage.storePVP;
+  markerX = localStorage.playerOne;
+  markerO = localStorage.playerTwo;
+  turnSwap = localStorage.turnKeep;
+  firstWins = parseInt(localStorage.storeFirstWins);
+  secondWins = parseInt(localStorage.storeSecondWins);
+  $('#left').text(firstWins);
+  $('#right').text(secondWins);
+}
+
+
+const resumeGameState = function() {
+  resumeGameData();
+
+  $('.Row1 .Col1').text(localStorage.square1);
+  $('.Row1 .Col2').text(localStorage.square2);
+  $('.Row1 .Col3').text(localStorage.square3);
+  $('.Row2 .Col1').text(localStorage.square4);
+  $('.Row2 .Col2').text(localStorage.square5);
+  $('.Row2 .Col3').text(localStorage.square6);
+  $('.Row3 .Col1').text(localStorage.square7);
+  $('.Row3 .Col2').text(localStorage.square8);
+  $('.Row3 .Col3').text(localStorage.square9);
+
+  for (let x = 0; x < 3; x++) {
+    for (let i = 0; i < 3; i++) {
+      if ($(`.Row${x+1} .Col${i+1}`).text() === markerX) {
+        $(`.Row${x+1} .Col${i+1}`).addClass('X');
+      } else if ($(`.Row${x+1} .Col${i+1}`).text() === markerO) {
+        $(`.Row${x+1} .Col${i+1}`).addClass('O');
+      }
+    }
+  }
+}
+
+
+// Used for debugging
+// const clearGameState = function() {
+//   for (let x = 0; x < 3; x++) {
+//     for (let i = 0; i < 3; i++) {
+//       localStorage.removeItem(`square${i+1+(x*3)}`);
+//     }
+//   }
+// }
+
+
+$(window).on( "unload", saveGameState);
+$(window).on( "load", resumeGameState);
+
+
+
+
+
+const testClick = function() {
+  $('.Row2 .Col2').trigger('click');
+}
+
+
+
+const bot = function() {
+  switch (true) {
+//Starting move
+  case board[4] === '':
+    $('.Row2 .Col2').trigger('click');
+    break;
+
+// Horizontal Offense
+  case board[0] === markerO && board[1] === markerO && board[2] === '':
+    $('.Row1 .Col3').trigger('click');
+    break;
+  case board[1] === markerO && board[2] === markerO && board[1] === '':
+    $('.Row1 .Col1').trigger('click');
+    break;
+  case board[0] === markerO && board[2] === markerO && board[1] === '':
+    $('.Row1 .Col2').trigger('click');
+    break;
+
+  case board[3] === markerO && board[4] === markerO && board[5] === '':
+    $('.Row2 .Col3').trigger('click');
+    break;
+  case board[4] === markerO && board[5] === markerO && board[3] === '':
+    $('.Row2 .Col1').trigger('click');
+    break;
+  case board[3] === markerO && board[5] === markerO && board[4] === '':
+    $('.Row2 .Col2').trigger('click');
+    break;
+
+  case board[6] === markerO && board[7] === markerO && board[8] === '':
+    $('.Row3 .Col3').trigger('click');
+    break;
+  case board[7] === markerO && board[8] === markerO && board[6] === '':
+    $('.Row3 .Col1').trigger('click');
+    break;
+  case board[6] === markerO && board[8] === markerO && board[7] === '':
+    $('.Row3 .Col2').trigger('click');
+    break;
+
+// Vertical Offense
+
+  case board[0] === markerO && board[3] === markerO && board[6] === '':
+    $('.Row3 .Col1').trigger('click');
+    break;
+  case board[3] === markerO && board[6] === markerO && board[0] === '':
+    $('.Row1 .Col1').trigger('click');
+    break;
+  case board[0] === markerO && board[6] === markerO && board[3] === '':
+    $('.Row2 .Col1').trigger('click');
+    break;
+
+  case board[1] === markerO && board[4] === markerO && board[7] === '':
+    $('.Row3 .Col2').trigger('click');
+    break;
+  case board[4] === markerO && board[7] === markerO && board[1] === '':
+    $('.Row1 .Col2').trigger('click');
+    break;
+  case board[1] === markerO && board[7] === markerO && board[4] === '':
+    $('.Row2 .Col2').trigger('click');
+    break;
+
+  case board[2] === markerO && board[5] === markerO && board[8] === '':
+    $('.Row3 .Col3').trigger('click');
+    break;
+  case board[5] === markerO && board[8] === markerO && board[2] === '':
+    $('.Row1 .Col3').trigger('click');
+    break;
+  case board[2] === markerO && board[8] === markerO && board[5] === '':
+    $('.Row2 .Col3').trigger('click');
+    break;
+
+// Diagonal Offense
+  case board[0] === markerO && board[4] === markerO && board[8] === '':
+    $('.Row3 .Col3').trigger('click');
+    break;
+  case board[4] === markerO && board[8] === markerO && board[0] === '':
+    $('.Row1 .Col1').trigger('click');
+    break;
+
+  case board[2] === markerO && board[4] === markerO && board[6] === '':
+    $('.Row3 .Col1').trigger('click');
+    break;
+  case board[4] === markerO && board[6] === markerO && board[2] === '':
+    $('.Row1 .Col3').trigger('click');
+    break;
+
+
+
+//Horizontal Defense
+  case board[0] === markerX && board[1] === markerX && board[2] === '':
+    $('.Row1 .Col3').trigger('click');
+    break;
+  case board[1] === markerX && board[2] === markerX && board[0] === '':
+    $('.Row1 .Col1').trigger('click');
+    break;
+  case board[0] === markerX && board[2] === markerX && board[1] === '':
+    $('.Row1 .Col2').trigger('click');
+    break;
+
+  case board[3] === markerX && board[4] === markerX && board[5] === '':
+    $('.Row2 .Col3').trigger('click');
+    break;
+  case board[4] === markerX && board[5] === markerX && board[3] === '':
+    $('.Row2 .Col1').trigger('click');
+    break;
+  case board[3] === markerX && board[5] === markerX && board[4] === '':
+    $('.Row2 .Col2').trigger('click');
+    break;
+
+  case board[6] === markerX && board[7] === markerX && board[8] === '':
+    $('.Row3 .Col3').trigger('click');
+    break;
+  case board[7] === markerX && board[8] === markerX && board[6] === '':
+    $('.Row3 .Col1').trigger('click');
+    break;
+  case board[6] === markerX && board[8] === markerX && board[7] === '':
+    $('.Row3 .Col2').trigger('click');
+    break;
+
+
+//Vertical Defense
+  case board[0] === markerX && board[3] === markerX && board[6] === '':
+    $('.Row3 .Col1').trigger('click');
+    break;
+  case board[3] === markerX && board[6] === markerX && board[0] === '':
+    $('.Row1 .Col1').trigger('click');
+    break;
+  case board[0] === markerX && board[6] === markerX && board[3] === '':
+    $('.Row2 .Col1').trigger('click');
+    break;
+
+  case board[1] === markerX && board[4] === markerX && board[7] === '':
+    $('.Row3 .Col2').trigger('click');
+    break;
+  case board[4] === markerX && board[7] === markerX && board[1] === '':
+    $('.Row1 .Col2').trigger('click');
+    break;
+  case board[1] === markerX && board[7] === markerX && board[4] === '':
+    $('.Row2 .Col2').trigger('click');
+    break;
+
+  case board[2] === markerX && board[5] === markerX && board[8] === '':
+    $('.Row3 .Col3').trigger('click');
+    break;
+  case board[5] === markerX && board[8] === markerX && board[2] === '':
+    $('.Row1 .Col3').trigger('click');
+    break;
+  case board[2] === markerX && board[8] === markerX && board[5] === '':
+    $('.Row2 .Col3').trigger('click');
+    break;
+
+
+
+//Diagonal Defense
+  case board[0] === markerX && board[4] === markerX && board[8] === '':
+    $('.Row3 .Col3').trigger('click');
+    break;
+  case board[4] === markerX && board[8] === markerX && board[0] === '':
+    $('.Row1 .Col1').trigger('click');
+    break;
+
+  case board[2] === markerX && board[4] === markerX && board[6] === '':
+    $('.Row3 .Col1').trigger('click');
+    break;
+  case board[4] === markerX && board[6] === markerX && board[2] === '':
+    $('.Row1 .Col3').trigger('click');
+    break;
+
+
+
+// Back up moves
+  case board[0] === '':
+    $('.Row1 .Col1').trigger('click');
+    break;
+  case board[1] === '':
+    $('.Row1 .Col2').trigger('click');
+    break;
+  case board[2] === '':
+    $('.Row1 .Col3').trigger('click');
+    break;
+  case board[3] === '':
+    $('.Row2 .Col1').trigger('click');
+    break;
+  case board[5] === '':
+    $('.Row2 .Col3').trigger('click');
+    break;
+  case board[6] === '':
+    $('.Row3 .Col1').trigger('click');
+    break;
+  case board[7] === '':
+    $('.Row3 .Col2').trigger('click');
+    break;
+  case board[8] === '':
+    $('.Row3 .Col3').trigger('click');
+    break;
+
+  default:
+    $('.winOutput').append(`<li id="draw">It's a Draw. Play again!</li>`);
+
+  }
+  checkWin();
+}
+
+
+
+
+
+
+
+//
